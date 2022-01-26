e695f9390a072ca1707bb0b2ace421bfdb49920f
Kieren <kieren@carderbee.com>
2022-01-26 13:12:38 +0000

    google feed files : update shipping : bug fix 2

--- apps/merchants/management/commands/generate_google_feed_files.py
+++ apps/merchants/management/commands/generate_google_feed_files.py
@@ -64,7 +64,7 @@ class Command(BaseCommand):
             'custom_label_2',
             'custom_label_3',
             'custom_label_4',
-            'shipping(country:price:min_transit_time:max_transit_time:max_handling_time)',
+            'shipping(country:price:min_handling_time:max_handling_time:min_transit_time:max_transit_time)',
         ]
 
         rows = [{
@@ -85,7 +85,7 @@ class Command(BaseCommand):
             'custom_label_2': None,
             'custom_label_3': None,
             'custom_label_4': None,
-            'shipping(country:price:min_transit_time:max_transit_time:max_handling_time)': str(product.google_shopping_delivery) + ':2',
+            'shipping(country:price:min_handling_time:max_handling_time:min_transit_time:max_transit_time)': product.google_shopping_delivery,
         } for product in products]
 
         return keys, rows
