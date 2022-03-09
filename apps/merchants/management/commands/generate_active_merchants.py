45c9bc3aca3e46b0341e3e22d9a1f3125da63fae
Kieren <kieren@carderbee.com>
2022-03-09 20:39:39 +0000

    generate active merchants command

---
+++ apps/merchants/management/commands/generate_active_merchants.py
@@ -0,0 +1,27 @@
+from lib2to3.pytree import Base
+import logging
+import csv
+from django.core.management.base import BaseCommand
+from apps.merchants.models import Merchant
+
+
+logger = logging.getLogger('merchants')
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        logger.info('generating active merchants csv file')
+        self._generate_file()
+
+    def _generate_file(self):
+        merchants = Merchant.objects.filter(
+            active=True,
+            approved=True
+        )
+
+        outfile_path = '/srv/fizzing-whizzbee/files/active_merchants.csv'
+        with open(outfile_path, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows([
+                merchant.domain, str(merchant.name).replace('Ltd.', '').strip(), 'GB'
+            ] for merchant in merchants if merchant.domain)
