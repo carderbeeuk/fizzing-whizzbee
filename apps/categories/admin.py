68a568fb1e037c0b12ec3801417eb34a6cb808a2
Kieren <kieren@carderbee.com>
2022-04-11 11:36:57 +0100

    categories admin : add offer count to columns

--- apps/categories/admin.py
+++ apps/categories/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Category, GoogleCategory
+from apps.products.models import Product
 
 # Register your models here.
 # admin.site.register(Category)
@@ -12,6 +13,16 @@ class GoogleCategoryAdmin(admin.ModelAdmin):
         'name',
         'cardinality',
         'google_category_id',
+        'get_offer_count',
         'parent_category',
         'active',
     )
+
+    @admin.display(description='Offer Count')
+    def get_offer_count(self, obj):
+        """gets the offer count for this merchant"""
+        offers = Product.objects.filter(
+            google_category_id=obj.google_category_id,
+            active=True
+        )
+        return len(offers)
